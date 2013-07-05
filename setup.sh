054d803eef9e671b6926482dc35425a5caaa70e0
Yuri Lee <yuri.lee24@gmail.com>
2013-07-04 21:51:38 -0500

    Changed editor to vim and added byobu

--- setup.sh
+++ setup.sh
@@ -20,24 +20,19 @@ npm install -g jshint
 # See: http://nodejs.org/api/repl.html#repl_repl
 sudo apt-get install -y rlwrap
 
-# Install emacs24
-# https://launchpad.net/~cassou/+archive/emacs
-sudo apt-add-repository -y ppa:cassou/emacs
+# Install vim
 sudo apt-get update
-sudo apt-get install -y emacs24 emacs24-el emacs24-common-non-dfsg
+sudo apt-get install -y vim
+
+#Install byobu
+sudo apt-get install -y byobu
 
 # git pull and install dotfiles as well
 cd $HOME
 if [ -d ./dotfiles/ ]; then
     mv dotfiles dotfiles.old
 fi
-if [ -d .emacs.d/ ]; then
-    mv .emacs.d .emacs.d~
-fi
 git clone https://github.com/startup-class/dotfiles.git
-ln -sb dotfiles/.screenrc .
 ln -sb dotfiles/.bash_profile .
 ln -sb dotfiles/.bashrc .
 ln -sb dotfiles/.bashrc_custom .
-ln -sf dotfiles/.emacs.d .
-
